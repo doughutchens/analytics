6bb84ab11ad451a5a4017b6845c442d0ea823277
Doug Hutchens <doug.hutchens@outlook.com>
2022-06-28 20:47:39 -0700

    add conference and team models

---
+++ hutchens/shared/Models/Conference.cs
@@ -0,0 +1,10 @@
+namespace Analytics.Hutchens.Shared
+{
+    public class Conference 
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string FullName {get; set;}
+        public string Logo {get; set;}
+    }
+}
---
+++ hutchens/shared/Models/Team.cs
@@ -0,0 +1,14 @@
+namespace Analytics.Hutchens.Shared
+{
+    public class Team
+    {
+        public int TeamId {get; set;}
+        public string Name {get; set;}
+        public string Abbreviation {get; set;}
+        public string Mascot {get; set;}
+        public string Color {get; set;}
+        public string AltColor {get; set;}
+        public string Logo {get; set;}
+        public string LogoDark {get; set;}
+    }
+}
