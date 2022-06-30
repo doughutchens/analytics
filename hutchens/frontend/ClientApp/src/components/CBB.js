1429f4b5aeda196d9be24237f47936f0fd482381
Doug Hutchens <doug.hutchens@outlook.com>
2022-06-29 22:11:13 -0700

    update for files

--- hutchens/frontend/ClientApp/src/components/CBB.js
+++ hutchens/frontend/ClientApp/src/components/CBB.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table';
+import Accordion from 'react-bootstrap/Accordion'
+
 
 export class CBB extends Component {
   static displayName = CBB.name;
@@ -7,6 +10,55 @@ export class CBB extends Component {
     return (
       <div>
         <h1>Men's College Basketball</h1>
+        <div>
+        <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+            <Accordion.Header>Top 25</Accordion.Header>
+            <Accordion.Body>
+                
+            </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum.
+            </Accordion.Body>
+        </Accordion.Item>
+        </Accordion>
+        </div>
+        <div>
+        <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </Table>
+        </div>
       </div>
     );
   }
